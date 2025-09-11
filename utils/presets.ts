ef9bce95142b3448410e7150194a8a541ca18ae7
ritzenjason-creator <ritzenjason@gmail.com>
2025-09-11 16:52:53 -0500

    Create presets.ts

---
+++ utils/presets.ts
@@ -0,0 +1,24 @@
+export const presets = [
+  {
+    name: 'Example 12',
+    Fs: 30,
+    Qts: 0.36,
+    Vas: 50,
+    Sd: 510,
+    Xmax: 14,
+    Re: 3.5,
+    Le: 1.9
+  },
+  {
+    name: 'Example 10',
+    Fs: 34,
+    Qts: 0.42,
+    Vas: 32,
+    Sd: 360,
+    Xmax: 12,
+    Re: 3.6,
+    Le: 1.6
+  }
+];
+
+export type DriverParams = typeof presets[number];
